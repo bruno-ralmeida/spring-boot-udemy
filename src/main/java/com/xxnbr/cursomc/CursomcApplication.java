21c55bc6155f6ed9e2b64155f0307b5ef8eaff95
Bruno Almeida <bruno-ralmeida@outlook.com>
2022-06-19 14:02:43 -0300

    feat (categorias) - Populando base de dados no inicio da aplicação para testes.

--- src/main/java/com/xxnbr/cursomc/CursomcApplication.java
+++ src/main/java/com/xxnbr/cursomc/CursomcApplication.java
@@ -1,13 +1,16 @@
 package com.xxnbr.cursomc;
 
 import com.xxnbr.cursomc.domain.Category;
+import com.xxnbr.cursomc.domain.Product;
 import com.xxnbr.cursomc.repositories.CategoryRepository;
+import com.xxnbr.cursomc.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 @EnableWebMvc
@@ -15,7 +18,9 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 public class CursomcApplication implements CommandLineRunner {
 
-	private final CategoryRepository repository;
+	private final CategoryRepository categoryRepository;
+
+	private final ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -23,17 +28,50 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		Product mouse = Product
+				.builder()
+				.id(null)
+				.name("Mouse")
+				.price(new BigDecimal(69.90))
+				.build();
+
+		Product teclado = Product
+				.builder()
+				.id(null)
+				.name("Teclado")
+				.price(new BigDecimal(80.0))
+				.build();
+
+		Product impressora = Product
+				.builder()
+				.id(null)
+				.name("Impressora")
+				.price(new BigDecimal(600.00))
+				.build();
+
+
 		Category informatica = Category
 				.builder()
+				.id(null)
 				.name("Informática")
+				.products(Arrays.asList(mouse,teclado,impressora))
 				.build();
+
 		Category escritorio = Category
 				.builder()
+				.id(null)
 				.name("Escritório")
+				.products(Arrays.asList(mouse,teclado))
 				.build();
 
-		repository.saveAll(Arrays.asList(informatica, escritorio));
+
+		mouse.getCategories().addAll(Arrays.asList(informatica,escritorio));
+		teclado.getCategories().addAll(Arrays.asList(informatica,escritorio));
+		impressora.getCategories().addAll(Arrays.asList(informatica));
 
 
+		categoryRepository.saveAll(Arrays.asList(informatica, escritorio));
+		productRepository.saveAll(Arrays.asList(mouse, teclado, impressora));
 	}
 }
