ce993b5b7bb3061fee6b8753840181be681e8e87
Bruno Almeida <bruno-ralmeida@outlook.com>
2022-06-19 12:28:01 -0300

    feat (categorias) - Populando base de dados no inicio da aplicação para testes.

--- src/main/java/com/xxnbr/cursomc/CursomcApplication.java
+++ src/main/java/com/xxnbr/cursomc/CursomcApplication.java
@@ -1,15 +1,39 @@
 package com.xxnbr.cursomc;
 
+import com.xxnbr.cursomc.domain.Category;
+import com.xxnbr.cursomc.repositories.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.util.Arrays;
+
 @EnableWebMvc
 @SpringBootApplication
-public class CursomcApplication {
+@RequiredArgsConstructor
+public class CursomcApplication implements CommandLineRunner {
+
+	private final CategoryRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Category informatica = Category
+				.builder()
+				.name("Informática")
+				.build();
+		Category escritorio = Category
+				.builder()
+				.name("Escritório")
+				.build();
+
+		repository.saveAll(Arrays.asList(informatica, escritorio));
+
+
+	}
 }
