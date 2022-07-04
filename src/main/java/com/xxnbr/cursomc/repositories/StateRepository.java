c904cbef8f5c68266374905d8375c109d8b37a33
Bruno Almeida <bruno-ralmeida@outlook.com>
2022-07-04 16:58:21 -0300

    feat (cliente) - Ajuste repositório - adicionada anotação @Repository

--- src/main/java/com/xxnbr/cursomc/repositories/StateRepository.java
+++ src/main/java/com/xxnbr/cursomc/repositories/StateRepository.java
@@ -2,6 +2,8 @@ package com.xxnbr.cursomc.repositories;
 
 import com.xxnbr.cursomc.domain.State;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StateRepository extends JpaRepository<State, Integer> {
 }
